7d8619105afc00aa872d725dc4c651255bc511da
Pedro Resende <pedroresende@mail.resende.biz>
2023-10-24 15:51:01 +0100

    test: increase test coverage

--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class TodoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save todo without title" do
+    todo = Todo.new
+    assert_not todo.save
+  end
+
+  test "should not save todo with title" do
+    todo = Todo.new(title: "test")
+    assert todo.save
+  end
+
+  test "should not save todo if provided date is before today" do
+    todo = Todo.new(title: "test", due_date: Date.today - 1.day)
+    assert_not todo.save
+  end
+
+  test "should save todo if provided date is today" do
+    todo = Todo.new(title: "test", due_date: Date.today)
+    assert todo.save
+  end
+
+  test "should save todo if provided date is tomorrow" do
+    todo = Todo.new(title: "test", due_date: Date.today + 1.day)
+    assert todo.save
+  end
 end
