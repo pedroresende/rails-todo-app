cec4d5f5e86162a19164d402238e76f545d2aa1d
Pedro Resende <951377+pedroresende@users.noreply.github.com>
2023-11-24 22:42:33 +0000

    fix: failing unit tests on the todo model

--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -3,31 +3,36 @@ require "test_helper"
 class TodoTest < ActiveSupport::TestCase
   test "should not save todo without title" do
     user = User.new()
-    todo = user.todos.new
+    todo_list = user.todo_lists.new()
+    todo = user.todo_lists.first.todos.new()
     assert_not todo.save
   end
 
   test "should not save todo with title" do
     user = User.new()
-    todo = user.todos.new(title: "test")
+    todo_list = user.todo_lists.new()
+    todo = user.todo_lists.first.todos.new(title: "test")
     assert todo.save
   end
 
   test "should not save todo if provided date is before today" do
     user = User.new()
-    todo = user.todos.new(title: "test", due_date: Date.today - 1.day)
+    todo_list = user.todo_lists.new()
+    todo = user.todo_lists.first.todos.new(title: "test", due_date: Date.today - 1.day)
     assert_not todo.save
   end
 
   test "should save todo if provided date is today" do
     user = User.new()
-    todo = user.todos.new(title: "test", due_date: Date.today)
+    todo_list = user.todo_lists.new()
+    todo = user.todo_lists.first.todos.new(title: "test", due_date: Date.today)
     assert todo.save
   end
 
   test "should save todo if provided date is tomorrow" do
     user = User.new()
-    todo = user.todos.new(title: "test", due_date: Date.today + 1.day)
+    todo_list = user.todo_lists.new()
+    todo = user.todo_lists.first.todos.new(title: "test", due_date: Date.today + 1.day)
     assert todo.save
   end
 end
