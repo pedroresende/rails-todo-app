1303594d386c2d87880c939625d28102416978e9
Pedro Resende <951377+pedroresende@users.noreply.github.com>
2023-11-26 18:27:41 +0000

    test: increase e2e tests

--- test/system/todo_lists.rb
+++ test/system/todo_lists.rb
@@ -1,15 +1,7 @@
 require "application_system_test_case"
 
 class LoginsTest < ApplicationSystemTestCase
-  test "visiting the index" do
-    visit "/"
-  
-    assert_selector "h1", text: "Login"
-    assert_selector :button, "Log in"
-    assert_selector "a", text: "Sign up"
-  end
-
-  test "login in" do
+  test "List todos" do
     visit "/"
 
     fill_in "user_email",	with: "hi@hi.com" 
@@ -18,5 +10,17 @@ class LoginsTest < ApplicationSystemTestCase
     click_on "Log in"
 
     assert_selector "h1", text: "Todo Lists"
+
+    visit "/todo_lists/new"
+
+    assert_selector "h1", text: "New Todo List"
+
+    fill_in "todo_list_title",	with: "Default todo list" 
+
+    click_on "Create"
+
+    assert text: "New Todo List"
+
+    assert_selector "a", text: "Edit"
   end
 end
